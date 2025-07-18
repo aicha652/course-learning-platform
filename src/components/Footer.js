3465cfc555240ae4c285a089bbe2449819c3848e
aicha652 <aichaazlf652@gmail.com>
2025-07-18 18:35:45 +0200

    add Styles to Home Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,45 @@
 import "../styles/Footer.css"
+import logo from "../assets/Mylogo.png"
+import { ImFacebook2 } from "react-icons/im"
+import { GrLinkedin } from "react-icons/gr"
+import { SiYoutubeshorts } from "react-icons/si";
+import { SiGmail } from "react-icons/si";
+
 
 export default function Footer() {
     return(
         <div className="footerContainer">
-            <div>
-                <h3>Main Pages</h3>
-                <p>About</p>
-                <p>Courses</p>
-                <p>Contact</p>
+            <div style={{ marginLeft: "30px" }}>
+                <img 
+                 src={logo} 
+                 alt="logo" 
+                 style={{ width: "100px", height: "100px", marginTop: "15px", marginLeft: "-300px"}}
+                />
+                <p style={{ width: "350px", marginLeft: "-30px", fontSize: "16px" }}>
+                    We are a modern e-learning platform committed to making education accessible, 
+                   flexible, and impactful. Learn at your own pace, anytime, anywhere.
+                </p>
+                <div className="icons">
+                    <ImFacebook2 />
+                    <GrLinkedin />
+                    <SiYoutubeshorts />
+                    <SiGmail />
+                </div>
             </div>
-            <div>
-                <h3>Popular Courses</h3>
+            <div style={{ marginTop: "15px" }}>
+                <h2 style={{ color: "blue" }}>Popular Courses</h2>
                 <p>Frontend Dev</p>
                 <p>Backend Dev</p>
                 <p>Full Stack Dev</p>
                 <p>Data Sience</p>
             </div>
-            <div>
-                <h3>Follow Us On</h3>
-                <p>Facebook</p>
-                <p>Instagram</p>
-                <p>Twitter</p>
-                <p>Linkedin</p>
-            </div>
+            <ul className="sectionThree">
+                <h2>Follow Us On</h2>
+                <li className="item">Facebook</li>
+                <li className="item">Instagram</li>
+                <li className="item">Twitter</li>
+                <li className="item">Linkedin</li>
+            </ul>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,23 @@ import { Link } from "react-router-dom";
 import { PiTarget } from "react-icons/pi";
 import { PiLampPendantLight } from "react-icons/pi";
 import { PiBrainLight } from "react-icons/pi";
+import AOS from "aos";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
+import { VscWorkspaceTrusted } from "react-icons/vsc";
+import { SiMinds } from "react-icons/si";
+import { FaRegHandshake } from "react-icons/fa6";
 
 export default function Home() {
+
+    useEffect(() => {
+        AOS.init({ duration: 1000 }); 
+    }, []);
     return(
         <div className="img-container">
             <img src={eLearning}/>
             <Navbar />
-            <div className="Text">
+            <div className="Text" data-aos="fade-left">
                 <h1>Midade AI Company AI & Data Experts</h1>
                 <h4>
                     A Saudi limited liability company specialized in providing comprehensive and
@@ -22,8 +32,9 @@ export default function Home() {
                 </h4>
                 <Link to="../contact"><button>Contact Us</button></Link>
             </div>
-            <div className="ValuesSection">
+            <div className="ValuesSection" data-aos="fade-up">
                 <p>Core Values</p>
+                <hr/>
                 <h5>
                    Our core values reflect our belief that learning should be accessible, 
                    empowering, and driven by real purpose. We strive to create a space where
@@ -31,27 +42,45 @@ export default function Home() {
                    the tools they need to grow — not just academically, but personally and
                    professionally.
                 </h5>
-                <div className="cards">
-                    <div className="card">
-                        <PiTarget />
-                        <h6>Learner-Centered Focus</h6>
-                        <h5>We prioritize the needs, goals, and success of every learner by
-                            creating accessible, engaging, and personalized learning experiences.
-                        </h5>
-                    </div>
-                    <div className="card">
-                        <PiLampPendantLight />
-                        <h6>Continuous Innovation</h6>
-                        <h5>We embrace innovation and adapt emerging technologies to
-                            deliver modern, effective, and interactive education.</h5>
-                    </div>
-                    <div className="card">
-                        <PiBrainLight />
-                        <h6 style={{ fontSize: "19px" }}>Knowledge Empowerment</h6>
-                        <h5>Our mission is to empower learners with practical skills,
-                            critical thinking, and lifelong learning habits that prepare
-                            them for real-world challenges.</h5>
-                    </div>
+            </div>
+            <div className="cards" style={{ marginLeft: "150px",width: "85%" }}>
+                <div className="card" data-aos="fade-up">
+                    <PiLampPendantLight />
+                    <h6>Continuous Innovation</h6>
+                    <h5>We embrace innovation and adapt emerging technologies to
+                        deliver modern, effective, and interactive education.</h5>
+                </div>
+                <div className="card" data-aos="fade-up">
+                    <PiBrainLight />
+                    <h6 style={{ fontSize: "19px" }}>Knowledge Empowerment</h6>
+                    <h5>Our mission is to empower learners with practical skills,
+                        critical thinking, and lifelong learning habits that prepare
+                        them for real-world challenges.</h5>
+                </div>
+                <div className="card" data-aos="fade-up">
+                    <VscWorkspaceTrusted />
+                    <h6 style={{ fontSize: "19px", marginLeft: "-100px" }}>Integrity & Trust</h6>
+                    <h5>We uphold honesty, transparency, and ethical practices in
+                        all our learning content, assessments, and interactions.</h5>
+                </div>
+                <div className="card" data-aos="fade-up">
+                    <SiMinds />
+                    <h6 style={{ fontSize: "19px", marginLeft: "-100px" }}>Growth Mindset</h6>
+                    <h5>We foster a culture of learning not only for users but within our team,
+                        continuously improving our platform and content based on feedback and
+                        outcomes.</h5>
+                </div>
+                <div className="card" data-aos="fade-up">
+                    <FaRegHandshake />
+                    <h6 style={{ fontSize: "19px", marginLeft: "-100px" }}>Equity & Inclusion</h6>
+                    <h5>We believe in equal opportunities for all. Our platform is committed to
+                        inclusivity—regardless of background, ability, or location.</h5>
+                </div>
+                <div className="card" data-aos="fade-up">
+                    <PiTarget />
+                    <h6>Learner-Centered Focus</h6>
+                    <h5 style={{ marginBottom: "-100px", marginTop: "-20px" }}>We prioritize the needs, goals, and success of every learner by
+                        creating accessible, engaging, and personalized learning experiences.</h5>
                 </div>
             </div>
             <Footer />
